7b19b41811f02c2992984266ef54a2d9d666f81d
tranglu <lu.t.t.trang@gmail.com>
2020-02-16 10:54:26 +0700

    lan2

---
+++ web_so_3/1.js
@@ -0,0 +1,15 @@
+$(functinon(){
+	$('.lentren').click(functinon(){
+		$('body').animate({scrollTopL:0});
+	});
+})
+
+
+$(functinon(){
+	$(window).scroll(functinon(){
+		$('.logo').addClass('chucam');
+		$('.navbar').addClass('nentrang')
+		.addClass('navbar-light')
+		.removeClass('navbar-dark');
+	})
+})
