843259aececfe8a98fd2d4a578eec43f47560d2a
tranglu <lu.t.t.trang@gmail.com>
2020-02-17 12:23:02 +0700

    lan3

--- web_so_3/1.js
+++ web_so_3/1.js
@@ -1,15 +1,34 @@
-$(functinon(){
-	$('.lentren').click(functinon(){
-		$('body').animate({scrollTopL:0});
+
+$(function(){
+	$('.lentren').click(function(){
+			$('body').animate({scrollTopL:0});
+			return false;
+		})
+});
+/*$(function(){
+	$(window).scroll(function(){
+		if($('body').scrollTop()>300){
+			$('.logo').addClass('chucam');
+			$('.navbar').addClass('nentrang')
+			.addClass('navbar-light')
+			.removeClass('navbar-dark');
+			$('.lentren').addclass('hienthi');
+		}
+		else if($('body').scrollTop()<=100) {
+			$('.logo').removeClass('chucam');
+			$('.navbar').removeClass('nentrang')
+			.removeClass('navbar-light')
+			.addClass('navbar-dark');
+			$('.lentren').removeClass('hienthi');
+		}
 	});
-})
-
-
-$(functinon(){
-	$(window).scroll(functinon(){
-		$('.logo').addClass('chucam');
-		$('.navbar').addClass('nentrang')
-		.addClass('navbar-light')
-		.removeClass('navbar-dark');
-	})
-})
+});*/
+$(function(){
+	$(window).scroll(function(){
+			$('.logo').addClass('chucam');
+			$('.navbar').addClass('nentrang')
+			.addClass('navbar-light')
+			.removeClass('navbar-dark');
+			/*$('.scroll').addClass('hienthi');*/
+	})	
+});
